be7b4e951bcbcce55e8ca9ff86491541c9c44846
Jack Jain <cirocelot@gmail.com>
2018-10-20 16:02:49 -0600

    added getters and setters to two properties

--- php/Classes/coin.php
+++ php/Classes/coin.php
@@ -8,7 +8,6 @@
 	namespace jjain2\DataDesign;
 
 	require_once(dirname(__DIR__, 2) . "./autoload.php");
-
 	class coin {
 		/**
 		 * id for the coin, primary key.
@@ -38,17 +37,48 @@
 
 		public function __construct($newCoinId, string $newCoinMarketCap, string $newCoinAllTimeHigh, string $newCoinVolume,
 	 string $newCoinSupply) {
-			$this -> setCoinId($newCoinId);
-			$this -> setCoinMarketCap($newCoinMarketCap);
-			$this -> setCoinAllTimeHigh($newCoinAllTimeHigh);
-			$this -> setCoinVolume($newCoinVolume);
-			$this -> setCoinSupply($newCoinSupply);
-		}
-		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			try {
+				$this->setCoinId($newCoinId);
+				$this->setCoinMarketCap($newCoinMarketCap);
+				$this->setCoinAllTimeHigh($newCoinAllTimeHigh);
+				$this->setCoinVolume($newCoinVolume);
+				$this->setCoinSupply($newCoinSupply);
+			} catch(\InvalidArgumentException|\RangeException|\Exception|\TypeError $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			}
 		}
 	public function getCoinId() : Uuid {
 		return($this->coinId);
+	}
+	public function setCoinId($newCoinId) : void {
+			try{
+				$uuid = self::validateUuid($newCoinId);
+			} catch(\InvalidArgumentException|\RangeException|\Exception|\TypeError $exception) {
+			}
+			$this->coinId=$uuid;
+	}
+
+		/**
+		 * @return string
+		 */
+		public function getCoinMarketCap(): string {
+			return $this->coinMarketCap;
+		}
+
+		/**
+		 * @param string $newCoinMarketCap
+		 */
+		public function setCoinMarketCap(string $newCoinMarketCap): void {
+			$newCoinMarketCap = trim($newCoinMarketCap);
+			$newCoinMarketCap = filter_var($newCoinMarketCap, FILTER_SANITIZE_STRING,
+				FILTER_FLAG_NO_ENCODE_QUOTES);
+			if(empty($newCoinMarketCap) === true) {
+				throw (new \InvalidArgumentException("Market cap is empty"));
+			}
+			if(stlen($newCoinMarketCap) >= 9) {
+				throw (new \RangeException("Content too large"));
+			}
+			$this->coinMarketCap = $newCoinMarketCap;
 	}
 	}
