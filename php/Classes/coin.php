857a3310c6697056fc62e1be4e6fdb66e56eb7bb
Jack Jain <cirocelot@gmail.com>
2018-10-22 14:56:27 -0600

    realized autoloader differences, fixed Uuid class from composer.json

--- php/Classes/coin.php
+++ php/Classes/coin.php
@@ -8,6 +8,8 @@
 	namespace jjain2\DataDesign;
 
 	require_once("./autoload.php");
+	require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+	use Ramsey\Uuid\Uuid;
 	class Coin {
 		use ValidateUuid;
 		/**
